451845bcdbb8bee436d6376ae47dce116e36c87a
cloworm <chloe.echikson@gmail.com>
2020-12-21 11:23:11 -0500

    added theme font and colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,11 @@ const Home = (): ReactElement => {
   return (
     <div>
       <Head>
+        <title>Countdown</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@1,700&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Red Hat Text', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        theme_grayishBlue: 'hsl(237, 18%, 59%)',
+        theme_softRed: 'hsl(345, 95%, 68%)',
+        theme_white: 'hsl(0, 0%, 100%)',
+        theme_darkDesaturatedBlue: 'hsl(236, 21%, 26%)',
+        theme_veryDarkBlue: 'hsl(235, 16%, 14%)',
+        theme_veryDarkMostlyBlackBlue: 'hsl(234, 17%, 12%)',
+      }
+    },
   },
   variants: {
     extend: {},
