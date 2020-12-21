3d98044feb74719d25cf8f63869b68c68f00f42e
Chloe Echikson <chloe.echikson@gmail.com>
2020-12-21 11:10:58 -0500

    Setup (#1)
    * added typescript
    * added tailwind
    * added eslint

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react'
+import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
+
+const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import { ReactElement } from 'react'
+
+const Home = (): ReactElement => {
+  return (
+    <div>
+      <Head>
+      </Head>
+
+      <main>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
