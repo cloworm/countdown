0a8f6253ec27edc3badb7e90418cda9c5959a320
cloworm <chloe.echikson@gmail.com>
2020-12-23 20:37:49 -0500

    adjusted countdown cards for mobile

--- components/CountdownCard.tsx
+++ components/CountdownCard.tsx
@@ -18,16 +18,16 @@ const CountdownCard = ({ id, label, current, previous }: Props): ReactElement =>
 
         {/* Top Half Static */}
         <div
-          className="relative rounded-t-lg h-20 w-44 overflow-hidden brightness-80"
+          className="relative rounded-t-lg h-10 w-20 lg:h-20 lg:w-44 overflow-hidden brightness-80"
         >
-          <p className="absolute top-8 left-0 right-0 text-center text-theme_softRed text-8xl font-bold">
+          <p className="absolute top-5 lg:top-8 left-0 right-0 text-center text-theme_softRed text-4xl lg:text-8xl font-bold">
             {typeof current === 'undefined' ? '' : padStart(current)}
           </p>
-          <svg width="11rem" height="5rem">
+          <svg width="100%" height="100%">
             <mask id={`${id}-m`} fill="#fff">
-              <rect id={`${id}-r`} width="11rem" height="5rem"/>
-              <circle r="8" fill="#000" cx="0" cy="5rem" />
-              <circle r="8" fill="#000" cx="11rem" cy="5rem" />
+              <rect id={`${id}-r`} width="100%" height="100%"/>
+              <circle r="8" fill="#000" cx="0" cy="100%" />
+              <circle r="8" fill="#000" cx="100%" cy="100%" />
             </mask>
             <use href={`#${id}-r`} fill="hsl(236, 21%, 26%)" mask={`url(#${id}-m)`} />
           </svg>
@@ -35,39 +35,39 @@ const CountdownCard = ({ id, label, current, previous }: Props): ReactElement =>
 
         {/* Bottom Half Static */}
         <div
-          className="relative rounded-b-lg h-20 w-44 overflow-hidden"
+          className="relative rounded-b-lg h-10 w-20 lg:h-20 lg:w-44 overflow-hidden"
         >
-          <p className="absolute bottom-8 left-0 right-0 text-center text-theme_softRed text-8xl font-bold" data-testid={id}>
+          <p className="absolute bottom-5 lg:bottom-8 left-0 right-0 text-center text-theme_softRed text-4xl lg:text-8xl font-bold" data-testid={id}>
             {typeof previous === 'undefined' ? '' : padStart(previous)}
           </p>
-          <svg width="11rem" height="5rem">
+          <svg width="100%" height="100%">
             <mask id={`${id}-m2`} fill="#fff">
-              <rect id={`${id}-r2`} width="11rem" height="5rem"/>
+              <rect id={`${id}-r2`} width="100%" height="100%"/>
               <circle r="8" fill="#000" cx="0" cy="0" />
-              <circle r="8" fill="#000" cx="11rem" cy="0" />
+              <circle r="8" fill="#000" cx="100%" cy="0" />
             </mask>
             <use href={`#${id}-r2`} fill="hsl(236, 21%, 26%)" mask={`url(#${id}-m2)`} />
           </svg>
         </div>
 
-        <p className="text-center text-theme_grayishBlue text font-bold tracking-widest pt-7">
+        <p className="text-xs lg:text-base text-center text-theme_grayishBlue lg:font-bold tracking-widest pt-7">
           {label}
         </p>
 
         { previous !== current &&
           <>
             <div
-              className="absolute top-0 left-0 rounded-t-lg h-20 w-44 overflow-hidden brightness-80 animate-flipTop origin-bottom preserve-3d backface-hidden"
+              className="absolute top-0 left-0 rounded-t-lg h-10 w-20 lg:h-20 lg:w-44 overflow-hidden brightness-80 animate-flipTop origin-bottom preserve-3d backface-hidden"
             >
               <div className="relative">
-                <p className="absolute top-8 left-0 right-0 text-center text-theme_softRed text-8xl font-bold">
+                <p className="absolute top-5 lg:top-8 left-0 right-0 text-center text-theme_softRed text-4xl lg:text-8xl font-bold">
                   {typeof previous === 'undefined' ? '' : padStart(previous)}
                 </p>
-                <svg width="11rem" height="5rem">
+                <svg width="100%" height="100%">
                   <mask id={`${id}-m`} fill="#fff">
-                    <rect id={`${id}-r`} width="11rem" height="5rem"/>
-                    <circle r="8" fill="#000" cx="0" cy="5rem" />
-                    <circle r="8" fill="#000" cx="11rem" cy="5rem" />
+                    <rect id={`${id}-r`} width="100%" height="100%"/>
+                    <circle r="8" fill="#000" cx="0" cy="100%" />
+                    <circle r="8" fill="#000" cx="100%" cy="100%" />
                   </mask>
                   <use href={`#${id}-r`} fill="hsl(236, 21%, 26%)" mask={`url(#${id}-m)`} />
                 </svg>
@@ -75,17 +75,17 @@ const CountdownCard = ({ id, label, current, previous }: Props): ReactElement =>
             </div>
 
             <div
-              className="absolute top-20 left-0 rounded-b-lg h-20 w-44 overflow-hidden animate-flipBottom origin-top preserve-3d backface-hidden"
+              className="absolute top-10 lg:top-20 left-0 rounded-b-lg h-10 w-20 lg:h-20 lg:w-44 overflow-hidden animate-flipBottom origin-top preserve-3d backface-hidden"
             >
               <div className="relative">
-                <p className="absolute bottom-8 left-0 right-0 text-center text-theme_softRed text-8xl font-bold">
+                <p className="absolute bottom-5 lg:bottom-8 left-0 right-0 text-center text-theme_softRed text-4xl lg:text-8xl font-bold">
                   {typeof current === 'undefined' ? '' : padStart(current)}
                 </p>
-                <svg width="11rem" height="5rem">
+                <svg width="100%" height="100%">
                   <mask id={`${id}-m2`} fill="#fff">
-                    <rect id={`${id}-r2`} width="11rem" height="5rem"/>
+                    <rect id={`${id}-r2`} width="100%" height="100%"/>
                     <circle r="8" fill="#000" cx="0" cy="0" />
-                    <circle r="8" fill="#000" cx="11rem" cy="0" />
+                    <circle r="8" fill="#000" cx="100%" cy="0" />
                   </mask>
                   <use href={`#${id}-r2`} fill="hsl(236, 21%, 26%)" mask={`url(#${id}-m2)`} />
                 </svg>
